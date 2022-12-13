acb9cefcfe8b675b62ebd1720529f165eecf6f6c
hayden-donnelly <hayden.git@proton.me>
2022-12-13 04:09:39 -0500

    Stable version.

--- scrape_earthexplorer.py
+++ scrape_earthexplorer.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 username = ''
@@ -65,8 +66,9 @@ for k in range(1, 1428):
         print('Opened download menu for result ' + current_result_number_string + '.')
 
         download_options_container = driver.find_element(By.ID, 'optionsContainer')
-        download_buttons = download_options_container.find_elements(By.CLASS_NAME, 'downloadButtons')
-        geotiff_download_button = download_buttons[2]
+        geotiff_download_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[7]/div[2]/div/div[2]/div[3]/div[1]/button')))
+        #download_buttons = download_options_container.find_elements(By.CLASS_NAME, 'downloadButtons')
+        #geotiff_download_button = download_buttons[2]
         geotiff_download_button.click()
         print('Started downloading result ' + current_result_number_string + '.')
 
