4fcf413a0f17f647185ed4d75bda94b9bc467709
hayden-donnelly <hayden.git@proton.me>
2023-01-07 13:19:55 -0500

    Fixed loop bug.

--- isolate_uncorrupted_heightmaps.py
+++ isolate_uncorrupted_heightmaps.py
@@ -12,17 +12,28 @@ model = keras.models.load_model('data/corrupted_heightmap_discriminator.h5')
 
 file_list = os.listdir(input_directory)
 
-for i in range(int(len(file_list) / 32)):
+corrupted_count = 0
+uncorrupted_count = 0
+
+print(len(file_list))
+
+for i in range(int(len(file_list) / batch_size)):
     batch = []
     for k in range(batch_size):
-        image = cv2.imread(input_directory + file_list[i + k], 0)
+        image = cv2.imread(input_directory + file_list[i * batch_size + k], 0)
         image = cv2.resize(image, dsize = (image_size, image_size), interpolation = cv2.INTER_AREA)
         batch.append(image)
 
     batch = np.asarray(batch)
     output = model.predict(batch)
 
-    for k in range(len(output)):
+    for k in range(batch_size):
         if(output[k][1] > output[k][0]):
             cv2.imwrite(output_directory + file_list[i + k] + '.png', batch[k])
-            print('Saving ' + file_list[i + k])
+            print('Saving ' + file_list[i * batch_size + k])
+            uncorrupted_count += 1
+        else:
+            corrupted_count += 1
+
+    print('Corrupted: ' + str(corrupted_count) + ', Uncorrupted: ' + str(uncorrupted_count))
+    print('Current batch: ', str(i))
